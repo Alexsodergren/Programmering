561b93701e7e7aff937c5a13935380c380f1da58
Alex Södergren <alexander.sodergren@elev.ga.ntig.se>
2022-08-31 14:22:29 +0200

    Initial commit

---
+++ Pr1/Pr1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+int points = 0;
+Console.WriteLine("Välkommen till Mickes transformers-frågesport!");
+
+// -----------------------------------------------------------------------------
+
+Console.WriteLine("1: Vad gillar Bakk att käka till lunch?");
+Console.WriteLine("a) Nyttig mat  b) Ben & Jerry  c) Normal mat");
+string answer1 = Console.ReadLine();
+if (answer1 == "b")
+{
+  points++;
+  Console.WriteLine("CORRECT");
+}
+else
+{
+  Console.WriteLine("INCORRECT");
+}
+
+// -----------------------------------------------------------------------------
+
+Console.WriteLine("2: Vad är bakks dröm?");
+Console.WriteLine("a) Att bli väl tränad  b) Att få en partner  c) Att bli smällfet");
+string answer2 = Console.ReadLine();
+if (answer2 == "c")
+{
+  points++;
+  Console.WriteLine("CORRECT");
+}
+else
+{
+  Console.WriteLine("INCORRECT");
+}
+
+// -----------------------------------------------------------------------------
+
+Console.WriteLine("3: Vad kommer bullen Bakk jobba med i framtiden?");
+Console.WriteLine("a) Spelutvecklare på Fortnite b) Sopgubbe  c) Civil ingenjör");
+string answer3 = Console.ReadLine();
+if (answer3 == "a")
+{
+  points++;
+  Console.WriteLine("CORRECT");
+}
+else
+{
+  Console.WriteLine("INCORRECT");
+}
+
+// -----------------------------------------------------------------------------
+
+Console.WriteLine($"Du fick {points} poäng.");
+if (points == 0)
+{
+  Console.WriteLine("Du har alltså ingen koll alls.");
+}
+else if (points < 3)
+{
+  Console.WriteLine("OK, du har hyfsad koll - eller gissade rätt en eller ett par gånger.");
+}
+else 
+{
+  Console.WriteLine("Grattis! Du hade alla rätt!");
+}
+
+Console.WriteLine("Tryck på valfri tangent för att avsluta.");
+Console.ReadKey();
